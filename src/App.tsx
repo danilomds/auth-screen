38226b70c58b102f5bb576c36a9309d082b5b052
Danilo Mendes <danilomds2007@gmail.com>
2021-03-23 23:44:11 -0300

    login page completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
+import { FiLock, FiMail, FiFacebook, FiTwitter } from 'react-icons/fi';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <img src="https://besthqwallpapers.com/Temp/23-3-2021/3d-blue-padlock-computer-security-encryption-3d-lock-security-blue-background-besthqwallpapers.com-2048x1536.jpg" alt="cadeado" />
+    <div className="container">
+      <h2>Login</h2>
+        <div className="form">
+          <div className="input-field">
+            <FiMail className="icons" />
+            <input type="text" name="email" id="input" placeholder=" E-mail" />
+            <div className="underline" ></div>
+          </div>
+
+          <div className="input-field">
+            <FiLock className="icons" />
+            <input type="password" name="password" id="password" placeholder=" Password" />
+            <div className="underline" ></div>
+          </div>
+            <input type="submit" value="Entrar" />
+        </div>  
+        
+        <div className="footer">
+          <span>Ou conecte-se com midias sociais</span>
+          <div className="social-field twitter">
+            <a href="#">
+              <FiTwitter className="icons" />
+              Login com Twitter
+              </a> 
+          </div>
+
+          <div className="social-field facebook">
+            <a href="#">
+              <FiFacebook className="icons" />
+              Login com Facebook
+              </a>
+          </div>
+
+        </div>     
+     </div>
+
     </div>
+    
+
+    
   );
 }
 
