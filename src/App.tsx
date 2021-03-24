5956deb08f25d0db7889450ae844f976f1ba6c37
Danilo Mendes <danilomds2007@gmail.com>
2021-03-24 01:21:16 -0300

    added functionality to show password

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import { useState } from 'react';
 import { FiLock, FiMail, FiFacebook, FiTwitter } from 'react-icons/fi';
+import { HiEye, HiEyeOff } from 'react-icons/hi';
 import './App.css';
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [show, setShow] = useState(false);
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    setShow(!show);
+  }
+
   return (
     <div className="main">
       <img src="https://besthqwallpapers.com/Temp/23-3-2021/3d-blue-padlock-computer-security-encryption-3d-lock-security-blue-background-besthqwallpapers.com-2048x1536.jpg" alt="cadeado" />
@@ -11,13 +21,40 @@ function App() {
         <div className="form">
           <div className="input-field">
             <FiMail className="icons" />
-            <input type="text" name="email" id="input" placeholder=" E-mail" />
+            <input 
+            type="text" 
+            value={email} 
+            id="input" 
+            placeholder=" Digite um email" 
+            onChange={e => setEmail(e.target.value)}
+            />
             <div className="underline" ></div>
           </div>
 
           <div className="input-field">
             <FiLock className="icons" />
-            <input type="password" name="password" id="password" placeholder=" Password" />
+            <input 
+            type={show ? "text" : "password"} 
+            value={password} 
+            id="password" 
+            placeholder=" Digite sua senha" 
+            onChange={e => setPassword(e.target.value)}
+            />
+            <div className="login-eye">
+              {show ? (
+                <HiEye                 
+                  size={20}  
+                  onClick={handleClick}
+                />             
+              ) : (
+                <HiEyeOff             
+                  size={20}  
+                  onClick={handleClick}
+                />
+              )}
+
+            </div>
+
             <div className="underline" ></div>
           </div>
             <input type="submit" value="Entrar" />
